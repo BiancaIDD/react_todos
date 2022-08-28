3d542ffb064530d232be4ad7334569469b264090
BiancaIDD <pastelitoesponjosoowo@gmail.com>
2022-08-28 00:17:32 -0600

    about terminado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import Landing from './landing_page/Landing'
 import './App.css';
+import About from './About/About';
 
 function App() {
   return (
     <div className="App">
-      <Landing/>
+      <About/>
     </div>
   );
 }
