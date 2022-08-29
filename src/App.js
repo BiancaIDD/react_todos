37c382ee38b96ac8ad5dd1cc3711bcf3f5d9e8fe
BiancaIDD <pastelitoesponjosoowo@gmail.com>
2022-08-28 18:23:05 -0600

    activando pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import './App.css';
 import SignUp from './pages/sign_up/Sign_up';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import About from './pages/About/About';
 import SignIn from './pages/sign_in/Sign_in';
 import Landing from './pages/landing_page/Landing';
 
+
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
       <Route index element={<Landing/>}/>
       <Route path='about' element={<About/>}/>
       <Route path='sign_in' element={<SignIn/>}/>
       <Route path='sign_up' element={<SignUp/>}/>
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
